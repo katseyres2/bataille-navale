a40bba968811731f1bfc4fc1bcdd01c675415d21
NadTum <akianadtum@gmail.com>
2023-02-12 17:16:39 +0100

    ajout de la méthode startSearchShip avec une récurcivité, tant qu'elle n'a pas trouvé un point qui n'est pas déjà listé, elle relance la méthode startSearchShip.

--- Bots/Logic.java
+++ Bots/Logic.java
@@ -1,14 +1,18 @@
 package Bots;
 
-import game.grid.Grid;
-
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 public class Logic extends Bots{
 
+    //récupération et création de l'objet sur une base default
     private ArrayList<ArrayList<String>> grid;
-
     private Integer[] coordonnees;
+    private List<int[]> alreadyTarget;
+
+    //pritate méthode pour générer des nombres aléatoires
+    private Random random;
 
     public Logic(ArrayList<ArrayList<String>> grid) {
         super(grid);
@@ -25,33 +29,41 @@ public class Logic extends Bots{
         if(coordonnees[0] == null && coordonnees[1] == null){
             //si c'est faux, il n'a pas touché de bâteau donc on ne fait rien
             //si c'est vrai, on indique la position du bateau.
-            if(startSearchShip()){
 
-            }
+            int[] targetPoint = startSearchShip();
+
+
+
         }else{
             //maintenant on vérifie si la direction n'est pas trouvé
             // un point de façon aléatoire mais qui doit se trouvé à côté du point trouvé
 
         }
 
-
-
         return true;
     }
 
     private String startSearchDirection(){
-
         return null;
     }
 
-    private Boolean startSearchShip(){
+    private int[] startSearchShip(){
 
+        int x = random.nextInt(getGrid().size());
+        int y = random.nextInt(getGrid().get(x).size());
 
+        int[] output = new int[2];
 
-        return false;
+        output[0] = x;
+        output[1] = y;
+
+        //vérifier si le point trouvé n'est pas déjà dans la liste des targetpoints sinon recommencer
+        if(alreadyTarget.contains(output)){
+            startSearchShip();
+        }
+        alreadyTarget.add(output);
+        return output;
     }
 
-
-
-
 }
+
