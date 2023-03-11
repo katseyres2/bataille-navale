7657493e7b2da9f54a479c9e571a658b3830b27e
NadTum <98037911+NadTumDEV@users.noreply.github.com>
2023-03-11 09:54:57 +0100

    UPDATE :
    ajout de variable de travail
    
    function qui check si un target est un bateau ou de l'eau : on doit encore faire un du code pour déterminer ce que donne à la function quand il faut rechercher un point
    ajout de la target pour éviter qui repointe vers la même target
    
    function pour ajouter la direction quand on trouve un bateau

--- Bots/Logic.java
+++ Bots/Logic.java
@@ -13,46 +13,73 @@ public class Logic extends Bots{
     //pritate méthode pour générer des nombres aléatoires
     private Random random;
 
-    public Logic(ArrayList<ArrayList<String>> grid, Integer[] coordonnees) {
+    public Logic(ArrayList<ArrayList<String>> grid, Integer[] coordonnees, List<int[]> alreadyTarget) {
         super(grid);
         this.coordonnees = coordonnees;
+        this.alreadyTarget = alreadyTarget;
     }
 
     public Boolean startTurn(ArrayList<ArrayList<String>> playerGrid){
 
         /*
         ETAPES D'UN TOUR :
-            1 on vérifie s'il n'a pas déjà trouvé un bâteau
-            2 s'il a trouvé un bâteau on voit si la direction est déjà trouvé / si pas on lance la procedure pour trouver un bâteau
+            Les fonctions ex : sendGrid, sont dans la classe objet bot
+            1 on vérifie s'il n'a pas déjà trouvé un bâteau / s'il a trouvé un bâteau on voit si la direction est déjà trouvé
+            2 si pas de bâteau, on doit en trouvé un, si le bot n'a pas pu toucher un bâteau, on retourne la grille
             3
          */
 
         if(coordonnees[0] == null && coordonnees[1] == null){
+
             //si c'est faux, il n'a pas touché de bâteau donc on ne fait rien
             //si c'est vrai, on indique la position du bateau.
-            int[] targetPoint = startSearchShip();
+            int x = random.nextInt(getGrid().size());
+            int y = random.nextInt(getGrid().get(x).size());
+            int[] targetPoint = checkSearchShip(x, y);
 
             if(targetPoint != null){
-                String direction = startSearchDirection();
+                String direction = startSearchDirection(random.nextInt(4));
+            }else{
+                sendGrid();
+                return false;
             }
-
         }else{
-            //maintenant on vérifie si la direction n'est pas trouvé
-            // un point de façon aléatoire mais qui doit se trouvé à côté du point trouvé
-
+            //maintenant, on vérifie si la direction n'est pas trouvée
+            // un point de façon aléatoire, mais qui doit se trouver à côté du point trouvé
         }
-
         return true;
     }
 
-    private String startSearchDirection(){
+    private String startSearchDirection(int direction){
+
+        int[] direction_form_interger = null;
+
+        switch (direction){
+            case 0:
+                direction_form_interger = new int[]{-1, 0};
+            case 2:
+                direction_form_interger = new int[]{1, 0};
+            case 3:
+                direction_form_interger = new int[]{0, -1};
+            case 1:
+                direction_form_interger = new int[]{0, 1};
+        }
+
+        checkTargetWithDirection(direction_form_interger);
+
         return null;
     }
 
-    private int[] startSearchShip(){
+    private Boolean checkTargetWithDirection(int[] newTarget){
 
-        int x = random.nextInt(getGrid().size());
-        int y = random.nextInt(getGrid().get(x).size());
+        if(checkSearchShip(newTarget[0], newTarget[1]) != null){
+
+        }
+
+        return false;
+    }
+
+    private int[] checkSearchShip(int x, int y){
 
         int[] output = new int[2];
 
@@ -61,14 +88,14 @@ public class Logic extends Bots{
 
         //vérifier si le point trouvé n'est pas déjà dans la liste des targetpoints sinon recommencer
         if(alreadyTarget.contains(output)){
-            startSearchShip();
+            //checkSearchShip(random.nextInt(getGrid().size()), random.nextInt(getGrid().get(x).size()));
         }
+
+        alreadyTarget.add(output);
 
         if(getGrid().get(x).get(y).equals(" ")){
             return null;
         }
-
-        alreadyTarget.add(output);
         return output;
     }
 
